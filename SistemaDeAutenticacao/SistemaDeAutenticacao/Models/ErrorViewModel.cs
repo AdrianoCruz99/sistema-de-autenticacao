723c6ef7a1cf49edfa78eb96d6f669f16120cb55
AdrianoCruz99 <adriano.two@outlook.com>
2024-04-01 11:46:34 -0300

    first save

---
+++ SistemaDeAutenticacao/SistemaDeAutenticacao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaDeAutenticacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
